f335210a025e91359c4ea2dffe62b6caf0d407da
Alan Van Art <alanv73@gmail.com>
2019-12-05 14:01:56 -0500

    initial commit

--- CatYears/ViewController.swift
+++ CatYears/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblOutput: UILabel!
+    @IBOutlet weak var txtAge: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblOutput.text = "";
     }
 
-
+    @IBAction func btnCalculate(_ sender: Any) {
+        let stringAge = txtAge.text ?? "";
+        let humanAge = Int(stringAge) ?? 0;
+        let catAge = humanAge * 7;
+        lblOutput.text = "Your Cat is\n\(catAge) Cat-Years Old";
+    }
+    
 }
 
